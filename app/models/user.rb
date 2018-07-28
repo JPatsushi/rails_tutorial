e5adad23ba09da1b700b47c1f56aeb7014c61873
AtsushiWatanabe <atsushi20180516@gmail.com>
2018-07-28 14:05:15 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy  
+                                   
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower #source: follower はなくてもよい
+  
   attr_accessor :remember_token, :activation_token, :reset_token
+  
+  
+  
   #before_save { self.email = email.downcase } 
   #before_save { email.downcase! }
   before_save   :downcase_email
@@ -78,10 +91,44 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
   
-   # 試作feedの定義
-   # 完全な実装は次章の「ユーザーをフォローする」を参照
+  # # 試作feedの定義
+  # # 完全な実装は次章の「ユーザーをフォローする」を参照
+  # def feed
+  #   Micropost.where("user_id = ?", id)
+  # end
+  
+  # # ユーザーのステータスフィードを返す
+  # def feed
+  #   Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+  # end
+  
+  # # ユーザーのステータスフィードを返す
+  # def feed
+  #   Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+  #   following_ids: following_ids, user_id: id)
+  # end
+  
+  # ユーザーのステータスフィードを返す。 サブセレクト
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+  
+  # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
   
   
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,5 +18,18 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    #演習追加12.2.2.3
+    assert_select 'div.pagination', count:1
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
+  
+  #演習追加12.2.2.3
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
   end
 end
+
