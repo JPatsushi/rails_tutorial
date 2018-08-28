6d32a0d4c06c94162489be2dc06ee18c67bf8702
AtsushiWatanabe <atsushi20180516@gmail.com>
2018-08-28 03:58:10 +0000

    header logo change

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title,         "成功の物語"
+    assert_equal full_title("Help"), "Help | 成功の物語"
   end
 end
